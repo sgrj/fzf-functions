956130a8a59e4d13688a10c5b75d1e754a7d54ee
leo <1005173+lrusnac@users.noreply.github.com>
2021-04-01 17:19:23 +0200

    use `$HOME` instead of `~`
    for consistency should use the same thing in the `ls` and `source` commands

--- activate-venv/activate-venv.bash
+++ activate-venv/activate-venv.bash
@@ -1,6 +1,6 @@
 function activate-venv() {
   local selected_env
-  selected_env=$(ls ~/.venv/ | fzf)
+  selected_env=$(ls $HOME/.venv/ | fzf)
 
   if [ -n "$selected_env" ]; then
     source "$HOME/.venv/$selected_env/bin/activate"
